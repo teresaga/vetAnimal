97fb88ce8c58307246470aaa17f0e5a9c2cb0669
Teresa <teresagalaviza@gmail.com>
2018-10-15 22:07:49 -0600

    second commit

---
+++ src/app/clients/addclients/addclients.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addclients',
+  templateUrl: './addclients.component.html',
+  styleUrls: ['./addclients.component.css']
+})
+export class AddclientsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
