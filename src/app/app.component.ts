e7859e59b7b99274e970ae23233b448f05a4ae9e
Teresa <teresagalaviza@gmail.com>
2018-10-19 21:57:19 -0600

    four commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+declare var jQuery:any;
+declare var $:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  /*PRUEBA DE JQUERY
+  public prueba(){
+    $(".header").css("background","red");
+  }*/
 }
