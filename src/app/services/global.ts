3a3a7b0fac44555d7ee7dbf925144f52b6212283
Teresa <teresagalaviza@gmail.com>
2018-12-09 21:45:22 -0700

    correciones 2

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,4 +1,4 @@
 export var GLOBAL = {
-    //url: 'https://vet-animal.herokuapp.com/api/'
-    url: 'http://localhost:3789/api/'
+    url: 'https://vet-animal.herokuapp.com/api/'
+    //url: 'http://localhost:3789/api/'
 }
